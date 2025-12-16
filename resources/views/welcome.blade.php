6054231e92b1d40c54fa0947c064a0c942e5128a
BaconHairBoi123 <imrealjojogoldenwind@gmail.com>
2025-12-16 20:14:19 +0800

    benerin halaman awal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,10 +6,10 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Ride Nusa')</title>
 
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/images/favicons/apple-touch-icon.png') }}" />
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/images/favicons/favicon-32x32.png') }}" />
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/favicons/favicon-16x16.png') }}" />
-    <link rel="manifest" href="{{ asset('assets/images/favicons/site.webmanifest') }}" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/favicons/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicons/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicons/favicon-16x16.png" />
+    <link rel="manifest" href="/assets/images/favicons/site.webmanifest" />
 
  <!-- fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -23,53 +23,54 @@
         href="https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
         rel="stylesheet">
     <!-- Core CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/animate.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/custom-animate.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/swiper.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/font-awesome-all.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/jarallax.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/jquery.magnific-popup.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/flaticon.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/owl.theme.default.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/nice-select.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/jquery-ui.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/aos.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/odometer.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/timePicker.css') }}" />
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="/assets/css/animate.min.css" />
+    <link rel="stylesheet" href="/assets/css/custom-animate.css" />
+    <link rel="stylesheet" href="/assets/css/swiper.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+    <!-- <link rel="stylesheet" href="/assets/css/font-awesome-all.css" /> -->
+    <link rel="stylesheet" href="/assets/css/jarallax.css" />
+    <link rel="stylesheet" href="/assets/css/jquery.magnific-popup.css" />
+    <!-- <link rel="stylesheet" href="/assets/css/flaticon.css" /> -->
+    <link rel="stylesheet" href="/assets/css/owl.carousel.min.css" />
+    <link rel="stylesheet" href="/assets/css/owl.theme.default.min.css" />
+    <link rel="stylesheet" href="/assets/css/nice-select.css" />
+    <link rel="stylesheet" href="/assets/css/jquery-ui.css" />
+    <link rel="stylesheet" href="/assets/css/aos.css" />
+    <link rel="stylesheet" href="/assets/css/odometer.min.css" />
+    <link rel="stylesheet" href="/assets/css/timePicker.css" />
 
     <!-- Module CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/css/module-css/slider.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/module-css/footer.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/module-css/sliding-text.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/module-css/services.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/module-css/about.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/module-css/booking.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/module-css/counter.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/module-css/listing.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/module-css/video.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/module-css/pricing.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/module-css/popular-car.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/module-css/testimonial.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/module-css/faq.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/module-css/team.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/module-css/call.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/module-css/download-app.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/module-css/brand.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/module-css/blog.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/module-css/lets-talk.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/module-css/process.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/module-css/why-choose.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/module-css/gallery.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/module-css/page-header.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/module-css/error.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/module-css/shop.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/module-css/contact.css') }}" />
+    <link rel="stylesheet" href="/assets/css/module-css/slider.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/footer.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/sliding-text.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/services.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/about.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/booking.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/counter.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/listing.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/video.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/pricing.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/popular-car.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/testimonial.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/faq.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/team.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/call.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/download-app.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/brand.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/blog.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/lets-talk.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/process.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/why-choose.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/gallery.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/page-header.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/error.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/shop.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/contact.css" />
 
     <!-- Template CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}" />
+    <link rel="stylesheet" href="/assets/css/style.css" />
+    <link rel="stylesheet" href="/assets/css/responsive.css" />
 
  
 
@@ -80,7 +81,7 @@
             <header class="main-header">
                 <div class="container d-flex justify-content-between align-items-center py-3">
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        <img src="{{ asset('assets/images/resources/logo_ridenusa_head.png') }}" alt="Ride Nusa" style="height:40px;">
+                        <img src="/assets/images/resources/logo_ridenusa_head.png" alt="Ride Nusa" style="height:40px;">
                     </a>
                     <div>
                         <a href="{{ route('login') }}"  class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal">Login</a>
@@ -96,7 +97,7 @@
 
                 <div class="item">
                     <div class="main-slider__bg"
-                        style="background-image: url('{{ asset('assets/images/backgrounds/1680x550_booking-one-bg.jpg') }}');">
+                        style="background-image: url('/assets/images/backgrounds/1680x550_booking-one-bg.jpg');">
                     </div><!-- /.slider-one__bg -->
                     <div class="container">
                         <div class="main-slider__content">
@@ -126,7 +127,7 @@
 
                 <div class="item">
                     <div class="main-slider__bg"
-                        style="background-image: url({{ asset('assets/images/backgrounds/xsr155.jpeg') }});">
+                        style="background-image: url(/assets/images/backgrounds/xsr155.jpeg);">
                     </div><!-- /.slider-one__bg -->
                     <div class="container">
                         <div class="main-slider__content">
@@ -156,7 +157,7 @@
 
                 <div class="item">
                     <div class="main-slider__bg"
-                        style="background-image: url('{{ asset('assets/images/backgrounds/nmaxturbo.jpeg') }}');">
+                        style="background-image: url('/assets/images/backgrounds/nmaxturbo.jpeg');">
                     </div><!-- /.slider-one__bg -->
                     <div class="container">
                         <div class="main-slider__content">
@@ -288,33 +289,33 @@
 
 
         <!-- Script yang dibutuhkan -->
-    <script src="{{ asset('assets/js/jquery-3.6.0.min.js') }}"></script>
-    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('assets/js/jarallax.min.js') }}"></script>
-    <script src="{{ asset('assets/js/jquery.ajaxchimp.min.js') }}"></script>
-    <script src="{{ asset('assets/js/jquery.appear.min.js') }}"></script>
-    <script src="{{ asset('assets/js/swiper.min.js') }}"></script>
-    <script src="{{ asset('assets/js/jquery.circle-progress.min.js') }}"></script>
-    <script src="{{ asset('assets/js/knob.js') }}"></script>
-    <script src="{{ asset('assets/js/jquery.magnific-popup.min.js') }}"></script>
-    <script src="{{ asset('assets/js/jquery.validate.min.js') }}"></script>
-    <script src="{{ asset('assets/js/wNumb.min.js') }}"></script>
-    <script src="{{ asset('assets/js/wow.js') }}"></script>
-    <script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script>
-    <script src="{{ asset('assets/js/jquery-ui.js') }}"></script>
-    <script src="{{ asset('assets/js/jquery.nice-select.min.js') }}"></script>
-    <script src="{{ asset('assets/js/jquery-sidebar-content.js') }}"></script>
-    <script src="{{ asset('assets/js/gsap/gsap.js') }}"></script>
-    <script src="{{ asset('assets/js/gsap/ScrollTrigger.js') }}"></script>
-    <script src="{{ asset('assets/js/gsap/SplitText.js') }}"></script>
-    <script src="{{ asset('assets/js/marquee.min.js') }}"></script>
-    <script src="{{ asset('assets/js/odometer.min.js') }}"></script>
-    <script src="{{ asset('assets/js/timePicker.js') }}"></script>
-    <script src="{{ asset('assets/js/typed-2.0.11.js') }}"></script>
-    <script src="{{ asset('assets/js/aos.js') }}"></script>
+    <script src="/assets/js/jquery-3.6.0.min.js"></script>
+    <script src="/assets/js/bootstrap.bundle.min.js"></script>
+    <script src="/assets/js/jarallax.min.js"></script>
+    <script src="/assets/js/jquery.ajaxchimp.min.js"></script>
+    <script src="/assets/js/jquery.appear.min.js"></script>
+    <script src="/assets/js/swiper.min.js"></script>
+    <script src="/assets/js/jquery.circle-progress.min.js"></script>
+    <script src="/assets/js/knob.js"></script>
+    <script src="/assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="/assets/js/jquery.validate.min.js"></script>
+    <script src="/assets/js/wNumb.min.js"></script>
+    <script src="/assets/js/wow.js"></script>
+    <script src="/assets/js/owl.carousel.min.js"></script>
+    <script src="/assets/js/jquery-ui.js"></script>
+    <script src="/assets/js/jquery.nice-select.min.js"></script>
+    <script src="/assets/js/jquery-sidebar-content.js"></script>
+    <script src="/assets/js/gsap/gsap.js"></script>
+    <script src="/assets/js/gsap/ScrollTrigger.js"></script>
+    <script src="/assets/js/gsap/SplitText.js"></script>
+    <script src="/assets/js/marquee.min.js"></script>
+    <script src="/assets/js/odometer.min.js"></script>
+    <script src="/assets/js/timePicker.js"></script>
+    <script src="/assets/js/typed-2.0.11.js"></script>
+    <script src="/assets/js/aos.js"></script>
 
     <!-- Template JS -->
-    <script src="{{ asset('assets/js/script.js') }}"></script>
+    <script src="/assets/js/script.js"></script>
          
     </body>
 </html>
